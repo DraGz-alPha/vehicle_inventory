0feec0f3b0d3c5e26027f56a5cda9b6a0dbd583b
Dustin Martens <dmartens.it@gmail.com>
2020-02-18 12:25:41 -0600

    Added parkingticket model to the vehicle inventory project

---
+++ app/models/parking_ticket.rb
@@ -0,0 +1,2 @@
+class ParkingTicket < ApplicationRecord
+end
---
+++ db/migrate/20200218182413_create_parking_tickets.rb
@@ -0,0 +1,16 @@
+class CreateParkingTickets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :parking_tickets do |t|
+      t.string :ticket_number
+      t.string :violation
+      t.string :street
+      t.string :latitude
+      t.string :longitude
+      t.double :discounted_price
+      t.double :full_price
+      t.datetime :issue_date
+
+      t.timestamps
+    end
+  end
+end
