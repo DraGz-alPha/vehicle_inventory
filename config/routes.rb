948a040504caba6ec5892d371a3de70125be5a4e
Dustin Martens <dmartens.it@gmail.com>
2020-02-18 13:00:55 -0600

    Added Vehicle controller with index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'vehicles/index'
+  get 'vehicles/show'
   get 'owners/index'
   get 'owners/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
