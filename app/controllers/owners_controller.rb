3f4689f4be7548b28aea62b7c5ccd2cf48928a40
Dustin Martens <dmartens.it@gmail.com>
2020-02-20 17:41:43 -0600

    Added data population to the owners index page

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 class OwnersController < ApplicationController
   def index
+    @owners = Owner.all
   end
 
   def show
+    # @owner = Owner.includes(:students, :teachers).find(params[:id])
+    @owner = Owner.find(params[:id])
   end
 end
