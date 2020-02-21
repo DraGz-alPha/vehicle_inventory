d3483bbece39374a8893a6c999e2b69d5e024c08
Dustin Martens <dmartens.it@gmail.com>
2020-02-20 22:51:05 -0600

    Made reference to vehicle collection for index and show methods in vehicle controller

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class VehiclesController < ApplicationController
   def index
+    @vehicles = Vehicle.all
   end
 
   def show
+    @vehicle = Vehicle.includes(:parking_tickets).find(params[:id])
   end
 end
