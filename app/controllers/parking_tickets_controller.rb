a5ae5382b655d69cf0c9c47f185923d7a9a4a1ae
Dustin Martens <dmartens.it@gmail.com>
2020-02-20 22:51:25 -0600

    Made reference to parking ticket collection for index and show methods in parking_ticket controller

--- app/controllers/parking_tickets_controller.rb
+++ app/controllers/parking_tickets_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class ParkingTicketsController < ApplicationController
   def index
+    @parking_tickets = ParkingTicket.all
   end
 
   def show
+    @parking_ticket = ParkingTicket.find(params[:id])
   end
 end
