1e6ed3bdd951026c0b6541a8608ea0288914c91f
Dustin Martens <dmartens.it@gmail.com>
2020-02-22 23:43:02 -0600

    Implemented pages table in db schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_223054) do
+ActiveRecord::Schema.define(version: 2020_02_23_052218) do
 
   create_table "owners", force: :cascade do |t|
     t.string "first_name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_02_20_223054) do
     t.string "last_name"
     t.string "prefix"
     t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
