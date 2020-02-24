a577a7fcfd6c0ccb21d43409f65ba2a9cf941d41
Dustin Martens <dmartens.it@gmail.com>
2020-02-24 12:54:11 -0600

    Added simple search to owners index view (search by last_name)

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -8,4 +8,10 @@ class OwnersController < ApplicationController
   def show
     @owner = Owner.includes(:vehicles, :parking_tickets).find(params[:id])
   end
+
+  # GET /owners/search_results
+  def search_results
+    @query = params[:query]
+    @owners = Owner.where('last_name LIKE ?', "%#{@query}%").page(params[:page])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,27 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'parking_tickets/index'
-  get 'parking_tickets/show'
-  get 'vehicles/index'
-  get 'vehicles/show'
-  get 'owners/index'
-  get 'owners/show'
+  # get 'owners/index'
+  # get 'owners/show'
+  resources :owners, only: :show do
+    collection do
+      get 'search_results'
+    end
+  end
 
-  resources :owners, only: :show
-  resources :vehicles, only: :show
-  resources :parking_tickets, only: :show
+  # get 'vehicles/index'
+  # get 'vehicles/show'
+  resources 'vehicles', only: %i[index show]
+
+  # get 'parking_tickets/index'
+  # get 'parking_tickets/show'
+  resources 'parking_tickets', only: %i[index show]
+
+  # get 'pages/index'
+  # get 'pages/show'
+  resources :pages
+
+  get ':permalink', to: 'pages#permalink', as: 'permalink'
 
   root 'owners#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
